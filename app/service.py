5d740628a40ce05558cc0233fe6daae3a3a0f867
root <stanislav.kovnacky@cosmos2.ru>
2025-04-29 10:56:29 +0300

    fix: parse_data bags

--- app/service.py
+++ app/service.py
@@ -1,31 +1,46 @@
 from datetime import datetime, timedelta
+from typing import List
+
 import requests
-from bs4 import BeautifulSoup
-from sqlalchemy import values
 
 from app.core.database import db_session
 from app.core.models import Currency
 
+import xml.etree.ElementTree as ET
+
 def fetch_data(date) -> str:
     url = f'https://cbr.ru/scripts/XML_daily.asp?date_req={date.strftime("%d/%m/%Y")}'
     response = requests.get(url)
-    response.encoding = 'utf-8' # Что бы не срало ISO-8859-1, решил попробовать utf-8
+    response.encoding = 'windows-1251' # Что бы не срало ISO-8859-1, решил попробовать utf-8
+
     return response.text
 
-def save_rates(data: str) -> None:
-    soup = BeautifulSoup(data, "xml")
-    valutes = soup.find_all("Valute")
-    currency_date = soup.ValCurs["Date"]
-    for valute in valutes:
-        try:
-            num_code = valute.NumCode.text
-            char_code = valute.CharCode.text
-            nominal = valute.Nominal.text
-            name = valute.Name.text
-            print(f"Saving: {num_code}, {char_code}, {nominal}, {name}")  # Логированием решил почекать чё оно выдаёт по символам
-        except Exception as e:
-            print(f"Error processing valute: {e}")
 
+def parse_data(xml_text: str):
+    root = ET.fromstring(xml_text)
+    valutes = []
+
+    for valute in root.findall('Valute'):
+        valutes.append({
+            'num_code': valute.find('NumCode').text,
+            'char_code': valute.find('CharCode').text,
+            'nominal': valute.find('Nominal').text,
+            'name': valute.find('Name').text,
+            'value': valute.find('Value').text
+        })
+
+    return valutes
+
+
+def save_rates(data: List, currency_date: datetime) -> None:
+    for dict_data in data:
+        #TODO: взять каждый элемент словаря
+        num_code = ""
+        char_code = ""
+        nominal = ""
+        name = ""
+        value = ""
+        #TODO: сделать проверку на currency_date, что бы не было повторяющихся данных
         db_session.add(
             Currency(
                 date=currency_date,
@@ -40,35 +55,15 @@ def save_rates(data: str) -> None:
     db_session.commit()
 
 
-def build_result_object(data):
-    soup = BeautifulSoup(data, "xml")
-    valutes = soup.find_all("Valute")
-    currency_date = soup.ValCurs["Date"]
-    result = []
-    for valute in valutes:
-        try:
-            valute_objetc = {
-                "num_code": valute.NumCode.text,
-                "char_code": valute.CharCode.text,
-                "nominal": valute.Nominal.text,
-                "name": valute.Name.text,
-                "value": valute.Value.text,
-            }
-
-            result.append(valute_objetc)
-        except Exception as e:
-            print(e)
-
-    return result
-
 def get_current_cours():
     today = datetime.today()
     yesterday = today - timedelta(days=1)
     data = fetch_data(yesterday)
-    save_rates(data)
-    current_cours = build_result_object(data)
+    parsed_data = parse_data(data)
+    #TODO: научить из списка преображать в сохранение в бд
+    save_rates(data=parsed_data,currency_date=yesterday)
 
-    return current_cours
+    return parsed_data
 
 def get_all_rates():
     return db_session.query(Currency).all()
